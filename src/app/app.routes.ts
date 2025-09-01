51ee022213e6596c6b655661eb883df9a802cfe8
Anh Luu <phongchoi@gmail.com>
2025-09-01 15:14:52 +0700

    Init project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const APP_ROUTE_PATHS = Object.freeze({
+  storage: {
+    path: 'storage',
+  },
+});
+
+export const routes: Routes = [
+  {
+    path: APP_ROUTE_PATHS.storage.path,
+    loadComponent: () => import('../storage/storage').then((m) => m.Storage),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: APP_ROUTE_PATHS.storage.path,
+  },
+];
